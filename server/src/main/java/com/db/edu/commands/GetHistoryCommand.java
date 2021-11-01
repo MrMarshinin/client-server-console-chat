2a2ac8d32cc612a3e38d881a8febf691e821bffb
aleksandra <adlovlac@gmail.com>
2021-11-01 11:37:44 +0300

    Imported saver

--- server/src/main/java/com/db/edu/commands/GetHistoryCommand.java
+++ server/src/main/java/com/db/edu/commands/GetHistoryCommand.java
@@ -1,5 +1,7 @@
 package com.db.edu.commands;
 
+import com.db.edu.Saver;
+
 public class GetHistoryCommand implements ChatCommand {
     @Override
     public void execute(Saver saver, Notifier notifier) {
