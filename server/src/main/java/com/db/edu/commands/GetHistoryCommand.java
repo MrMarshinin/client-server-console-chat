df759d75053e66dfb91b64c5db8dd87a3269506e
aleksandra <adlovlac@gmail.com>
2021-11-01 13:52:47 +0300

    Added default implementation for commands

--- server/src/main/java/com/db/edu/commands/GetHistoryCommand.java
+++ server/src/main/java/com/db/edu/commands/GetHistoryCommand.java
@@ -6,6 +6,7 @@ import com.db.edu.Saver;
 public class GetHistoryCommand implements ChatCommand {
     @Override
     public void execute(Saver saver, Notifier notifier) {
-
+        System.out.println("Execute history command");
+        notifier.sendErrorMessage("Not implemented yet");
     }
 }
