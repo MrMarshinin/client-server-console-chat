1f8c0b7d914711f1f66c314ba759cbd53c5d9c5e
George <gshervashidze1999@yandex.ru>
2021-11-01 14:18:18 +0300

    multithread server

--- server/src/main/java/com/db/edu/Server.java
+++ server/src/main/java/com/db/edu/Server.java
@@ -8,32 +8,39 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import java.util.concurrent.Executors;
 
 public class Server {
     public static void main(String[] args) {
-        try (final ServerSocket listener = new ServerSocket(9999);
-            final Socket connection = listener.accept();
-            final DataInputStream input = new DataInputStream(
-                    new BufferedInputStream(connection.getInputStream()));
-            final DataOutputStream out = new DataOutputStream(
-                    new BufferedOutputStream(connection.getOutputStream()));
-        ) {
-            Parser parser = new Parser();
-
-            Saver saver = new FileSaver();
-            Notifier notifier = new Notifier(out);
-
+        try {
+            final ServerSocket listener = new ServerSocket(9999);
             while (true) {
-                final String commandString = input.readUTF();
-                System.out.println("Got from client: " + commandString);
+                Executors.newFixedThreadPool(5000).execute(() -> {
+                    try {
+                        Socket connection = listener.accept();
+                        final DataInputStream input = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
+                        final DataOutputStream out = new DataOutputStream(new BufferedOutputStream(connection.getOutputStream()));
+                        Parser parser = new Parser();
 
-                try {
-                    parser.parse(commandString).execute(saver, notifier);
-                } catch(IllegalArgumentException exception) {
-                    System.out.println(exception.getMessage());
-                    notifier.sendErrorMessage(exception.getMessage());
-                }
+                        Saver saver = new FileSaver();
+                        Notifier notifier = new Notifier(out);
 
+                        while (true) {
+                            final String commandString = input.readUTF();
+                            System.out.println("Got from client: " + commandString);
+
+                            try {
+                                parser.parse(commandString).execute(saver, notifier);
+                            } catch(IllegalArgumentException exception) {
+                                System.out.println(exception.getMessage());
+                                notifier.sendErrorMessage(exception.getMessage());
+                            }
+
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
