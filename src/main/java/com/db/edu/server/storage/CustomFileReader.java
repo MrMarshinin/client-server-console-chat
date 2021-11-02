3a6509b5f5feb080ec69548b898ce4d3041f55a3
George <gshervashidze1999@yandex.ru>
2021-11-02 11:05:34 +0300

    fix bugs

--- src/main/java/com/db/edu/server/storage/CustomFileReader.java
+++ src/main/java/com/db/edu/server/storage/CustomFileReader.java
@@ -1,6 +1,5 @@
 package com.db.edu.server.storage;
 
-import com.db.edu.server.ConnectionHandler;
 import com.db.edu.server.entity.Message;
 import com.db.edu.server.entity.Printer;
 import org.slf4j.Logger;
@@ -14,12 +13,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomFileReader implements Reader {
-    private final String HISTORY_FILE_NAME;
+    private final String historyFileName;
 
     private static final Logger log = LoggerFactory.getLogger(CustomFileReader.class);
 
     public CustomFileReader() {
-        HISTORY_FILE_NAME = "history.txt";
+        historyFileName = "history.txt";
     }
 
     @Override
@@ -32,10 +31,10 @@ public class CustomFileReader implements Reader {
         List<String> result = new ArrayList<>();
         try {
 
-            File file = new File(HISTORY_FILE_NAME);
+            File file = new File(historyFileName);
             checkExist(file);
 
-            BufferedReader br = new BufferedReader(new FileReader(HISTORY_FILE_NAME));
+            BufferedReader br = new BufferedReader(new FileReader(historyFileName));
 
             String line = br.readLine();
 
@@ -61,8 +60,7 @@ public class CustomFileReader implements Reader {
 
     public Message tryCreateMessage(String line) {
         try {
-            Message message = new Message(line);
-            return message;
+            return new Message(line);
         } catch (IllegalArgumentException e) {
             log.error("Invalid format for message");
             return null;
