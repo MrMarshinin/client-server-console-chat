5034cc13a6c066077001606e215eed45ed8536e0
ignat-mishura <ignat.mishura@gmail.com>
2021-11-02 13:43:42 +0300

    GetHistoryCommandTest changed

--- src/test/java/com/db/edu/server/commands/GetHistoryCommandTest.java
+++ src/test/java/com/db/edu/server/commands/GetHistoryCommandTest.java
@@ -1,6 +1,7 @@
 package com.db.edu.server.commands;
 
 import com.db.edu.server.Notifier;
+import com.db.edu.server.entity.Message;
 import com.db.edu.server.entity.User;
 import com.db.edu.server.storage.Reader;
 import com.db.edu.server.storage.Saver;
@@ -9,22 +10,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@Disabled
+
 class GetHistoryCommandTest {
 
     Reader reader;
-    Logger logger;
     GetHistoryCommand command;
     Notifier notifier;
     Saver saver;
     User user;
+    String message;
 
     @BeforeEach
     void setUp() {
@@ -32,22 +35,24 @@ class GetHistoryCommandTest {
         saver = mock(Saver.class);
         notifier = mock(Notifier.class);
         user = mock(User.class);
-        logger = mock(Logger.class);
         command = new GetHistoryCommand(reader);
+//        message = new ArrayList<String>(Arrays.asList("a", "b"));
+        message = "c";
     }
 
     @Test
     void execute() {
-        List<String> messages = new ArrayList<String>();
-        messages.add("a");
-        messages.add("b");
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message("2021-11-01T22:57:29.523, a, a"));
+//        messages.add(new Message("2021-11-01T22:57:29.523, a, b"));
 
         when(user.getRoom()).thenReturn("");
-//        when(reader.readSpecificRoom("")).thenReturn(messages);
+        when(reader.readSpecificRoom("")).thenReturn(messages);
 
         command.execute(saver, notifier, user);
 
-        verify(logger).info("Execute history command");
-        verify(notifier, times(messages.size())).sendPersonalMessage(new String(), user);
+//        verify(notifier, times(messages.size())).sendPersonalMessage(Matchers.in(messages).toString(), user);
+//        verify(notifier).sendPersonalMessage("2021-11-01T22:57:29.523, a, b", user);
+        verify(notifier).sendPersonalMessage("2021-11-01T22:57:29.523, a, a", user);
     }
 }
