e7b606d28c28f6231f78ef3fee28bff2b12fc506
George <gshervashidze1999@yandex.ru>
2021-11-02 10:19:08 +0300

    fix some bugs

--- src/main/java/com/db/edu/client/Client.java
+++ src/main/java/com/db/edu/client/Client.java
@@ -1,6 +1,5 @@
 package com.db.edu.client;
 
-import com.db.edu.server.ConnectionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +7,14 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-    private static final String host = "127.0.0.1";
-    private static final int port = 9999;
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 9999;
 
-    private static final Logger log = LoggerFactory.getLogger(ConnectionHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
         try {
-            final Socket socket = new Socket(host, port);
+            final Socket socket = new Socket(HOST, PORT);
             final DataInputStream input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             final DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
@@ -24,6 +23,7 @@ public class Client {
             final MessageHandler messageHandler = new MessageHandler(reader, input, out);
             messageHandler.handle();
             messageHandler.shutdown();
+            socket.close();
         } catch (IOException e) {
             log.error(e.getMessage());
         }
