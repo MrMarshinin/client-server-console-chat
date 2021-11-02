91305f4793b47a95485909682c846d3e95bce674
George <gshervashidze1999@yandex.ru>
2021-11-02 11:08:49 +0300

    fix code smells

--- src/main/java/com/db/edu/server/entity/Printer.java
+++ src/main/java/com/db/edu/server/entity/Printer.java
@@ -1,6 +1,10 @@
 package com.db.edu.server.entity;
 
 public class Printer {
+    private Printer() {
+
+    }
+
     public static void print(String string) {
         System.out.println(string);
     }
